4d7d84951fb503a262ccb797789c9f17759a2ad1
Forbes Gray <fnbg75@gmail.com>
2018-07-10 16:36:27 +0100

    Move Loading to own component

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 (function () {
   var uri = document.baseURI
 
+  var LoadingComponent = Vue.component('LoadingComponent', {
+    name: 'loading-component',
+    template: `
+      <div class="loading">
+        <div class="spinner">
+          <svg class="lds-blocks" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            <rect x="19" y="19" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="40" y="19" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.125s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="61" y="19" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.25s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="19" y="40" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.875s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="61" y="40" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.375s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="19" y="61" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.75s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="40" y="61" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.625s" calcMode="discrete"></animate>
+            </rect>
+            <rect x="61" y="61" width="20" height="20" fill="#1c4595">
+              <animate attributeName="fill" values="#e76a24;#1c4595;#1c4595" keyTimes="0;0.125;1" dur="1s" repeatCount="indefinite" begin="0.5s" calcMode="discrete"></animate>
+            </rect>
+          </svg>
+        </div>
+      </div>
+    `
+  })
+
   var SVGComponent = Vue.component('SVGComponent', {
     name: 'svg-component',
     props: ['childMap', 'mouseover', 'mouseout'],
@@ -119,7 +155,8 @@
       }
     },
     components: {
-      'svg-component': SVGComponent
+      'svg-component': SVGComponent,
+      'loading-component': LoadingComponent
     }
   })
 })()
